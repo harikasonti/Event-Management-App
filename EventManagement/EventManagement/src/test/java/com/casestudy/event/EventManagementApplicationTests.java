47d071bd2260a4b9bd2e5b0e21120a1b998291c4
Sonti <harika.sonti@capgemini.com>
2022-11-28 21:18:16 +0530

    first commit

---
+++ EventManagement/EventManagement/src/test/java/com/casestudy/event/EventManagementApplicationTests.java
@@ -0,0 +1,17 @@
+package com.casestudy.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+
+@ExtendWith(MockitoExtension.class)
+class EventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
